04f7450cac86f7708f6c508e35d7c79e6fa91a67
aabchyein <abcqaz369@naver.com>
2023-06-15 10:56:57 +0900

    수정

--- src/terminalpolls/Pollpeople.java
+++ src/terminalpolls/Pollpeople.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Pollpeople {
-    public static void main(String[] args) {
+    public String pollpeople() {
         try {
             String url = "jdbc:mysql://127.0.0.1:3306/db_polls";
             String user = "root";
@@ -19,32 +19,34 @@ public class Pollpeople {
 
             Statement statement = connection.createStatement();
             String query = "";
-
             // 작업 키 입력
             Scanner scanner = new Scanner(System.in);
+            HashMap<String, String> resNumberMap = new HashMap<>();
             String workKey = " ";
             while (!workKey.equals("E") || !workKey.equals("Exit")) {
                 System.out.print("선택입력 : ");
                 workKey = scanner.nextLine();
                  if (workKey.equals("P")) {
                     System.out.println("- 설문자 가능 명단(가입 완료)");
-                    query = "SELECT respondents\n" + //
-                            "FROM RESPONDENTS";
+                    query = "SELECT T_RESP.RESPONDENTS, T_RESP.RESPONDENTS_ID\n" + //
+                            "FROM respondents AS T_RESP\n" + //
+                            ";";
                     ResultSet resultSet0 = statement.executeQuery(query);
                     int number = 1;
-                    HashMap<String, String> resNumberMap = new HashMap<>();
+                    
                     while (resultSet0.next()) {
                         System.out.print(number + ". " + resultSet0.getString("RESPONDENTS") + ", ");
-                        resNumberMap.put(String.valueOf(number), resultSet0.getString("RESPONDENTS"));
+                        resNumberMap.put(String.valueOf(number), resultSet0.getString("RESPONDENTS_ID"));
                         number = number + 1;
                     }
+                        resNumberMap.get(String.valueOf(number));
 
                     int cnt = 0;
                     while (cnt == 0) {
                         System.out.println();
                         System.out.print("- 설문자 번호 입력 : ");
                         int ans = scanner.nextInt();
-
+                        String Rid = resNumberMap.get(String.valueOf(ans));
                         if (ans > 4) {
                             System.out.print("-Error- 확인 후 입력 필요");
                             cnt = 0;
